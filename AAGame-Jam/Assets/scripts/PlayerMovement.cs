2e20518015770994fe8dd8bc134a00693e95dc8f
ExAmbitionAl <exambition1@gmail.com>
2018-03-10 12:20:43 +0100

    Walking animation start

--- AAGame-Jam/Assets/scripts/PlayerMovement.cs
+++ AAGame-Jam/Assets/scripts/PlayerMovement.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     Rigidbody rb;
     float h, v;
-    public float speed=1f;
+    public float speed = 1f;
 
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
-        rb.transform.Translate(new Vector3(h * speed, 0 , v * speed) * Time.deltaTime);
+        rb.transform.Translate(new Vector3(h * speed, 0, v * speed) * Time.deltaTime);
 
-	}
+    }
+
+    void playerRotate()
+    {
+
+    }
 }
