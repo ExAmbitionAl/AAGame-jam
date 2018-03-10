5471d237bdf83b9a8184b20c95d55dce34d3dcb5
ExAmbitionAl <exambition1@gmail.com>
2018-03-10 12:08:07 +0100

    Player movement

---
+++ AAGame-Jam/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Rigidbody rb;
+    float h, v;
+    public float speed=1f;
+
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        rb.transform.Translate(new Vector3(h * speed, 0 , v * speed) * Time.deltaTime);
+
+	}
+}
